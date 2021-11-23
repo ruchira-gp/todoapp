96a02ae7ab1883feec21cd21b7931916cfcab255
ruchira-gp <ruchira.gp99@gmail.com>
2021-11-23 13:52:47 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id=navi>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#" style="padding-left:20px">To-do</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+  
+      <b-navbar-nav class="ms-auto">
+        <b-nav-item href="#">View Progress</b-nav-item>
+        <b-nav-item href="#">Done</b-nav-item>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <b-button variant="outline-info" class="mb-2" id="addButton">
+      <b-icon icon="plus-circle" aria-hidden="true"></b-icon> Add task
+    </b-button>
+</div>
 </template>
+<style scoped>
+b-navbar-nav,b-nav-item,navbar-collapse{
+  padding:10px
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#nav-collapse{
+  padding:10px
 }
+#addButton{
+    padding: 30px;
+    display:block;
+    
+    text-align:center;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+}
+
 </style>
