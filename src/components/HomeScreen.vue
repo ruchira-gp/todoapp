bd11d92b52152992e24ed28e7f6c13fb70d23ff2
ruchira-gp <ruchira.gp99@gmail.com>
2021-11-25 02:11:04 +0530

    button added

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -8,13 +8,14 @@
     <b-collapse id="nav-collapse" is-nav>
   
       <b-navbar-nav class="ms-auto">
-        <b-nav-item href="#">View Progress</b-nav-item>
-        <b-nav-item href="#">Done</b-nav-item>
+        <b-nav-item href="#" @click="modalShow=!modalShow">View Progress</b-nav-item>
+        <b-nav-item href="#" @click="modalShow=!modalShow">Done</b-nav-item>
 
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
   <div style="height:70px"></div>
+  <b-modal hide-footer title="Feature yet to be added" v-model="modalShow"></b-modal>
   <slot>
 
   </slot>
@@ -27,6 +28,16 @@
 import AddTask from './AddTask.vue'
 export default {
   components: { AddTask },
+  data(){
+    return{
+      modalShow: false,
+    }
+  },
+  methods: { 
+    shw(){
+      this.modalShow=true;
+    }
+  }
     
 }
 </script>
